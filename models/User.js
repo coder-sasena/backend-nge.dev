bf4207825efe78818f07a87438cba75a196158f5
Fnc Git <asigitj1@gmail.com>
2024-02-15 08:28:34 +0700

    API fixing bug

--- models/User.js
+++ models/User.js
@@ -29,5 +29,9 @@ UserSchema.methods.generateJWT = async function () {
   });
 };
 
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
 const User = model("User", UserSchema);
 export default User;

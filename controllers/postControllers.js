20c688bcafba2d10f8bf9d815e0da4b6f6a25471
Fnc Git <asigitj1@gmail.com>
2024-06-02 00:01:41 +0700

    get post detail API

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -124,6 +124,12 @@ const getPost = async (req, res, next) => {
             match: {
               check: true,
             },
+            populate: [
+              {
+                path: "user",
+                select: ["avatar", "name"],
+              },
+            ],
           },
         ],
       },
